d12c8a6a374233ba100d5d06f0b13a69b144b1fb
Rishi Durgapal <rishidurgapal@example.com>
2025-11-23 14:44:09 +0530

    deployment issues

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -5,7 +5,7 @@ export const initSocket = async () =>{
         'force new connection': true,
         reconnectionAttempts : 'Infinity',
         timeout: 10000,
-        transports: ['websocket'],
+        transports: ['polling', 'websocket'],
     };
     return io(process.env.REACT_APP_BACKEND_URL, options);
 }
